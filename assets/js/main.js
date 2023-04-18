0c33e4a5b1096c626dc37624e46083d1e8e68479
minhajm12 <minhajatholi222@gmail.com>
2023-04-18 16:42:18 +0530

    service

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+$('.services').owlCarousel({
+    loop:true,
+    margin:20,
+   autoplay:true,
+   autoplayTimeout:3000,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
--- home.php
+++ home.php
@@ -8,9 +8,18 @@
     <title>Powerline Solution</title>
 
     <!-- bootstrap CDN link -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet">
 
+    <!-- Owl Carousel CDN -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css" />
+
+    <!-- Font Awesome CDN -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
+
+    <!-- Home Style -->
     <link rel="stylesheet" href="assets/css/home.css">
+
 
 </head>
 
@@ -33,7 +42,7 @@
 
                     <!-- Toggle button -->
                     <button class="navbar-toggler px-0" type="button" data-bs-toggle="collapse" data-bs-target="#navbarExample4" aria-controls="navbarExample4" aria-expanded="false" aria-label="Toggle navigation">
-                        <i class="fas fa-bars"></i>
+                        <i class="fa-solid fa-bars-staggered"></i>
                     </button>
 
                     <!-- Collapsible wrapper -->
@@ -173,7 +182,7 @@
                                             <div class="col-md-6 col-xl-4 mb-3 mb-xl-0 first-col bsnss">
                                                 <div class="pt-2">
                                                     <div class="bg-image hover-overlay shadow-1-strong rounded ripple mb-4" data-bs-ripple-color="light">
-                                                        <img src="assets/img/mega-menu/Industrial-automation-main.jpg" class="img-fluid" />
+                                                        <img src="assets/img/mega-menu/hydraulics.jpeg" class="img-fluid" />
                                                         <a href="#!">
                                                             <div class="mask"></div>
                                                         </a>
@@ -252,37 +261,41 @@
                                                     </a>
                                                 </div>
                                             </div> -->
-                                            <div class="col-md-6 col-xl-3 mb-3 mb-md-0 scnd-col">
+                                            <div class="col-md-6 col-xl-8 mb-3 mb-md-0 scnd-col">
                                                 <div class="list-group list-group-flush">
                                                     <p class="mb-0 list-group-item text-uppercase font-weight-bold">
                                                         Our Products
                                                     </p>
-                                                    <a href="" class="list-group-item list-group-item-action"><i class="fas fa-caret-right pe-2"></i>Senetry Equipment Corp </a>
-                                                    <a href="" class="list-group-item list-group-item-action"><i class="fas fa-caret-right pe-2"></i>Tuthill</a>
-                                                    <a href="" class="list-group-item list-group-item-action"><i class="fas fa-caret-right pe-2"></i>Klaus Union</a>
-                                                    <a href="" class="list-group-item list-group-item-action"><i class="fas fa-caret-right pe-2"></i>Bernard Controls</a>
-                                                    <a href="" class="list-group-item list-group-item-action"><i class="fas fa-caret-right pe-2"></i>FTI</a>
-                                                    <a href="" class="list-group-item list-group-item-action"><i class="fas fa-caret-right pe-2"></i>Edward Vaccum Solutions</a>
-                                                    <a href="" class="list-group-item list-group-item-action"><i class="fas fa-caret-right pe-2"></i>Blackmer - PSG Dover</a>
-                                                    <a href="" class="list-group-item list-group-item-action"><i class="fas fa-caret-right pe-2"></i>Ebsray - PSG Dover</a>
-                                                </div>
-                                            </div>
-                                            <div class="col-md-6 col-xl-5 third-col">
-                                                <div class="list-group list-group-flush">
-                                                    <p class="mt-2 list-group-item text-uppercase font-weight-bold"></p>
-                                                    <a href="" class="list-group-item list-group-item-action"><i class="fas fa-caret-right pe-2"></i>Milton Roy</a>
-                                                    <a href="" class="list-group-item list-group-item-action"><i class="fas fa-caret-right pe-2"></i>Griswold - Psg Dover</a>
-                                                    <a href="" class="list-group-item list-group-item-action"><i class="fas fa-caret-right pe-2"></i>Mouvex - Psg Dover</a>
-                                                    <a href="" class="list-group-item list-group-item-action"><i class="fas fa-caret-right pe-2"></i>Argal Pumps</a>
+                                                    <a href="" class="list-group-item list-group-item-action"><i class="fas fa-caret-right pe-2"></i>EPC </a>
+                                                    <a href="" class="list-group-item list-group-item-action"><i class="fas fa-caret-right pe-2"></i>Industrial Automation</a>
+                                                    <a href="" class="list-group-item list-group-item-action"><i class="fas fa-caret-right pe-2"></i>Industrial Drives</a>
+                                                    <a href="" class="list-group-item list-group-item-action"><i class="fas fa-caret-right pe-2"></i>Power Backup</a>
+                                                    <a href="" class="list-group-item list-group-item-action"><i class="fas fa-caret-right pe-2"></i>Power System Services</a>
+                                                    <a href="" class="list-group-item list-group-item-action"><i class="fas fa-caret-right pe-2"></i>Hydraulics And fa-pencilumatics</a>
                                                 </div>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
                             </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">About US</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Career</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Contact</a>
+                            </li>
 
                         </ul>
                         <!-- Left links -->
+                    </div>
+
+                    <div class="country-change-icon">
+                        <a href="#">
+                            <i class="fa-solid fa-earth-americas"></i>
+                        </a>
                     </div>
                     <!-- Collapsible wrapper -->
                 </div>
@@ -290,14 +303,157 @@
             </nav>
         </header>
 
+        <div id="banner">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-6 left">
+                        <p class="top-head">POWERLINE SOLUTIONS</p>
+                        <h1>Join Hands with us to Make Wonders</h1>
+                        <!-- <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis expedita maiores, doloremque delectus nesciunt.</p> -->
+                        <a href="" class="main-btn">Read More</a>
+                    </div>
+                    <div class="col-lg-6 right"></div>
+                </div>
+            </div>
+        </div>
+
+        <!-- ABOUT SECTION -->
+        <section id="aboutsection">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-6 left">
+                        <div class="img-div">
+                            <img src="assets/img/about-section/abt-img-1.png" alt="" class="abt-img-1 img-fluid">
+                            <img src="assets/img/about-section/abt-img-2.png" alt="" class="abt-img-2 img-fluid">
+                            <img src="assets/img/about-section/abt-img-3.png" alt="" class="abt-img-3 img-fluid">
+                        </div>
+                    </div>
+                    <div class="col-lg-6 right">
+                        <p class="top-head">About us</p>
+                        <h1 class="main-head">Powerline Solutions</h1>
+                        <p class="content-para">Since 2005 from inception, we have completed more than 240 projects across 4 countries, the majority which is hustle in nature. Our company is built on leadership and a relentless adherence to our values, the core of which are Ethics, safety, quality, and employee welfare. These values are what we believe, we practice, what we expect, what we deliver to our customers and the society we live in.</p>
+                        <a href="" class="main-btn">Read More</a>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- ABOUT SECTION END -->
+
+
+        <!-- SERVICE SECTION -->
+        <section id="servicesection">
+            <div class="container">
+                <h2 class="main-head mx-2">Our Services</h2>
+                <div class="services owl-carousel">
+                    <div class="service-cards">
+                        <img src="assets/img/service-card-1.jpg" alt="">
+                        <div class="card-details">
+                            <h2>EPC</h2>
+                            <p>Engineering, Procurement, and Construction of Civil, Mechanical, Electrical, Instrumentation, Controls and Automation disciplines.</p>
+                            <a href="" class="service-btn">View Service</a>
+                        </div>
+                    </div>
+                    <div class="service-cards">
+                        <img src="assets/img/banner/industrial-image.jpg" alt="">
+                        <div class="card-details">
+                            <h2>Interior Projects</h2>
+                            <p>Turnkey Interior fitout and Electromechanical Service Provider.</p>
+                            <a href="" class="service-btn">View Service</a>
+                        </div>
+                    </div>
+                    <div class="service-cards">
+                        <img src="assets/img/service-card-2.jpg" alt="">
+                        <div class="card-details">
+                            <h2>Power Backup</h2>
+                            <p>Experts in Base design to commissioning for Batteries, Rectifiers & Chargers, UPS.</p>
+                            <a href="" class="service-btn">View Service</a>
+                        </div>
+                    </div>
+                    <div class="service-cards">
+                        <img src="assets/img/service-card-1.jpg" alt="">
+                        <div class="card-details">
+                            <h2>Power System Services</h2>
+                            <p>Professionals in design, retrofitting, upgradation, Preventive Maintenance, Trouble Shooting, Testing & commissioning of Electrical Systems including Switchgears, Relays & Transformers.</p>
+                            <a href="" class="service-btn">View Service</a>
+                        </div>
+                    </div>
+                    <div class="service-cards">
+                        <img src="assets/img/service-card-1.jpg" alt="">
+                        <div class="card-details">
+                            <h2>Hydraulics and Pneumatics</h2>
+                            <p>Highly reliable hydraulic & pneumatic products and services at a competitive price and fastest possible time.</p>
+                            <a href="" class="service-btn">View Service</a>
+                        </div>
+                    </div>
+                    <div class="service-cards">
+                        <img src="assets/img/service-card-1.jpg" alt="">
+                        <div class="card-details">
+                            <h2>Oil & Gas</h2>
+                            <p>World class branded products & services for various specialized pumps & Blowers.</p>
+                            <a href="" class="service-btn">View Service</a>
+                        </div>
+                    </div>
+                    <div class="service-cards">
+                        <img src="assets/img/service-card-1.jpg" alt="">
+                        <div class="card-details">
+                            <h2>Polymer And Rubber</h2>
+                            <p>One of the leading distributor of all types of rubber products as per standards and customer requirements.</p>
+                            <a href="" class="service-btn">View Service</a>
+                        </div>
+                    </div>
+                    <div class="service-cards">
+                        <img src="assets/img/service-card-1.jpg" alt="">
+                        <div class="card-details">
+                            <h2>Power Quality</h2>
+                            <p>With Extensive experience and proficiency in the field of Power Quality, offers a full spectrum of Power Quality services and products.</p>
+                            <a href="" class="service-btn">View Service</a>
+                        </div>
+                    </div>
+                    <div class="service-cards">
+                        <img src="assets/img/service-card-1.jpg" alt="">
+                        <div class="card-details">
+                            <h2>Industrial Automation</h2>
+                            <p>Quality provider of automation products, solutions, and services with a good team of dedicated personnel’s</p>
+                            <a href="" class="service-btn">View Service</a>
+                        </div>
+                    </div>
+                    <div class="service-cards">
+                        <img src="assets/img/service-card-1.jpg" alt="">
+                        <div class="card-details">
+                            <h2>Industrial Drives</h2>
+                            <p>Solution provider for any design, rectification, troubleshooting, training, testing & commissioning in MV/LV AC & DC Drives.</p>
+                            <a href="" class="service-btn">View Service</a>
+                        </div>
+                    </div>
+                    <div class="service-cards">
+                        <img src="assets/img/service-card-1.jpg" alt="">
+                        <div class="card-details">
+                            <h2>Medical Division</h2>
+                            <p>At the forefront in delivering the best Medical technologies by partnering with innovative companies to provide the modern health care services. We have the potential to transform the invention as well as generate innovative solutions beyond the standard of care.</p>
+                            <a href="" class="service-btn">View Service</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- SERVICE SECTION END -->
 
     </div>
 
 
+    <!-- J-QUERY CDN -->
+    <script src="https://code.jquery.com/jquery-3.6.4.js" integrity="sha256-a9jBBRygX1Bh5lt8GZjXDzyOB+bWve9EiO7tROUtj/E=" crossorigin="anonymous"></script>
 
     <!-- Bootstrap CDN Script -->
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.min.js" integrity="sha384-zYPOMqeu1DAVkHiLqWBUTcbYfZ8osu1Nd6Z89ify25QV9guujx43ITvfi12/QExE" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.min.js" integrity="sha384-Y4oOpwW3duJdCWv5ly8SCFYWqFDsfob/3GkgExXKV4idmbt98QcxXYs9UoXAB7BZ" crossorigin="anonymous"></script>
+
+    <!-- OWL Carousel -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.js"></script>
+
+    <!-- MAIN JS -->
+    <script src="assets/js/main.js"></script>
 
 </body>
 
