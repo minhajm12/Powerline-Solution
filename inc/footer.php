e13fc1fb8616cc80efba091382289c18ee7a43e3
minhajm12 <minhajatholi222@gmail.com>
2023-05-11 10:48:39 +0530

    link

--- inc/footer.php
+++ inc/footer.php
@@ -4,10 +4,10 @@
             <div class="col-lg-3 col-md-6 my-lg-0 my-md-0 my-4 foot-logo" data-aos="fade-up" data-aos-duration="1000">
                 <img src="assets/img/logo.gif" class="img-fluid footer-logo" alt="" width="50%">
             </div>
-            <div class="col-lg-2 col-md-6 my-lg-0 my-md-0 my-3 links" data-aos="fade-up"  data-aos-delay="200" data-aos-duration="1000">
+            <div class="col-lg-2 col-md-6 my-lg-0 my-md-0 my-3 links" data-aos="fade-up" data-aos-delay="200" data-aos-duration="1000">
                 <h2>Usefull Links</h2>
                 <ul>
-                    <a href="">
+                    <a href="home.php">
                         <li>Home</li>
                     </a>
                     <a href="">
@@ -19,25 +19,25 @@
                     <a href="">
                         <li>Products</li>
                     </a>
-                    <a href="">
+                    <a href="about.php">
                         <li>About</li>
                     </a>
-                    <a href="">
+                    <a href="career.php">
                         <li>Career</li>
                     </a>
-                    <a href="">
+                    <a href="contact.php">
                         <li>Contact Us</li>
                     </a>
                 </ul>
             </div>
-            <div class="col-lg-3 col-md-6 my-lg-0 my-md-4 my-3 location" data-aos="fade-up"  data-aos-delay="400" data-aos-duration="1000">
+            <div class="col-lg-3 col-md-6 my-lg-0 my-md-4 my-3 location" data-aos="fade-up" data-aos-delay="400" data-aos-duration="1000">
                 <h2>Location</h2>
                 <div>
                     <i class="fa-sharp fa-solid fa-location-dot"></i>
                     <p>Office No.606, 6 th Floor,<br>Building No-100,<br>Markaz Al-Mana Building, Al Matar Street, P.O Box # 203357 Doha, State of Qatar</p>
                 </div>
             </div>
-            <div class="col-lg-3 col-md-6 my-lg-0 my-md-4 my-3 contact" data-aos="fade-up"  data-aos-delay="600" data-aos-duration="1000">
+            <div class="col-lg-3 col-md-6 my-lg-0 my-md-4 my-3 contact" data-aos="fade-up" data-aos-delay="600" data-aos-duration="1000">
                 <h2>Contact</h2>
                 <div>
                     <i class="fa-solid fa-envelope"></i>
